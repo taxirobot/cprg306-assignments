7a5b37a703afe1db2fe9f711715a7fe05d1b93a6
taxirobot <77999020+taxirobot@users.noreply.github.com>
2026-03-13 09:28:03 -0600

    Week 4

--- app/page.js
+++ app/page.js
@@ -3,10 +3,11 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-    <h1>CPRG 306: Web Development 2 - Assignments</h1>
-    <Link href = "/week-2">Go to Week 2 → </Link>
-    <Link href = "/week-3">Go to Week 3 → </Link>
-    </>
+    <main>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <Link href = "/week-2">Go to Week 2 → </Link>
+      <Link href = "/week-3">Go to Week 3 → </Link>
+      <Link href = "/week-4">Go to Week 4 → </Link>
+    </main>
   );
 }
