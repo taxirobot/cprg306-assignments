e6a7171760db886d83038f6f80b96f2db10f9f85
taxirobot <77999020+taxirobot@users.noreply.github.com>
2026-03-24 14:01:18 -0600

    removed week-8

--- app/week-8/layout.js
+++
@@ -1,9 +0,0 @@
-// "use client";
-// import { AuthContextProvider } from "./_utils/auth-context";
- 
-
-// const Layout = ({ children }) => {
-//   return <AuthContextProvider>{children}</AuthContextProvider>;
-// };
- 
-// export default Layout;
